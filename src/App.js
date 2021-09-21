bb82331a46d6b1ec16936f5b252ff8e2a062aa32
OmarAlbarghouthi <omar.qb8077@gmail.com>
2021-09-21 22:35:31 +0300

    almost done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+
+    super(props);
+    this.state = {
+      locationResl: {},
+      getDatainfo: [],
+      searchQuery: '',
+      showAll: false
+    }
+
+  }
+
+  cityLoc = async (e) => {
+    e.preventDefault();
+
+
+    await this.setState({
+      searchQuery: e.target.city.value
+    })
+
+    try {
+      let locURL = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_LOCATION_KEY}&q=${this.state.searchQuery}&format=json`
+      let locResult = await axios.get(locURL)
+      console.log(locResult.data[0]);
+
+
+      this.setState({
+        locationResl: locResult.data[0],
+        showAll: true
+
+      })
+      // getting weather data
+      let wthUrl = `http://localhost:3001/weather?cityName=${this.state.searchQuery}`;
+      let weatherData = await axios.get(wthUrl);
+      console.log(weatherData.data);
+     await this.setState({
+
+        getDatainfo: weatherData.data
+      })
+      // console.log(getDatainfo);
+
+
+    } catch {
+      console.log('something went wrong');
+    }
+
+  }
+  render() {
+    return (
+      <div>
+        <h2>city explorer app </h2>
+
+
+        <form onSubmit={this.cityLoc}>
+          <input type="text" name='city' />
+          <input type="submit" value='render all' />
+
+
+        </form>
+        {this.state.showAll &&
+
+          <>
+            <p>city Name : {this.state.searchQuery}</p>
+            <p>latitude: {this.state.locationResl.lat}</p>
+            <p>longitude:{this.state.locationResl.lon}</p>
+            {
+            this.state.getDatainfo.map(element => {
+              return( <div>
+                    <p>Date{element.date}</p>
+                    <p>description {element.description}</p>
+              </div>
+              )
+            })
+          }
+          
+
+
+            <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_LOCATION_KEY}&center=${this.state.locationResl.lat},${this.state.locationResl.lon}&zoom=10`} alt="" />
+          </>
+        }
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
